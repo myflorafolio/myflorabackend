fe851cebe60008fac0aa0e4dfd52b290d85fc777
myflorafolio <heathergaylefournier@gmail.com>
2026-03-22 11:28:02 -0600

    server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import OpenAI from "openai";
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
